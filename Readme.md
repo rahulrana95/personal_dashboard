606d3ec86bfa11087590a8f51d5cbd28074f1649
Rahul Rana <rahul.rana@rivigo.com>
2019-01-27 20:56:05 +0530

    Change

--- Readme.md
+++ Readme.md
@@ -3,7 +3,9 @@ Pesonal Dashboard
 # TODO
 
 - Write a component to save resources (links of websites codes etc)
+  - Add node api to add dta in resources.json file
   - Implement search by title and description
   - Add filter by type, tags, categoreis
   - Add logic for last seen or last clicked something like this
   - Add node api to add dta in resources.json file
+  - Add read or un read and fav filter in it
--- src/components/features/Resources.js
+++ src/components/features/Resources.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 import ComponentHeader from "../ComponentHeader";
-import { List, Avatar, Icon, Row, Button } from "antd";
+import { List, Avatar, Icon, Row, Button, Tabs, Radio } from "antd";
 import Drawer from "components/features/Drawer";
 import AddResourceForm from "components/features/AddResourcesForm";
 import rData from "../../domless/resources.json";
+
+const TabPane = Tabs.TabPane;
+const IconText = ({ type, text, icon, style }) => (
+  <span>
+    <Icon type={type} style={{ marginRight: 8, ...style }} />
+    {text}
+  </span>
+);
+
+const HashTags = ({ list }) => {
+  return list.map(item => {
+    return <span>#{item}</span>;
+  });
+};
 
 class Resources extends Component {
   constructor() {
@@ -13,8 +27,6 @@ class Resources extends Component {
     };
   }
   render() {
-    const { resources } = this.props;
-    console.log(this.state);
     return (
       <div className="component-container">
         <ComponentHeader>Resources</ComponentHeader>
@@ -28,38 +40,73 @@ class Resources extends Component {
               content={<AddResourceForm />}
             />
           </Row>
-
-          <List
-            itemLayout="vertical"
-            size="large"
-            pagination={{
-              onChange: page => {
-                console.log(page);
-              },
-              pageSize: 10
-            }}
-            dataSource={resources}
-            renderItem={item => (
-              <List.Item
-                key={item.title}
-                extra={
-                  <img
-                    width={150}
-                    alt="logo"
-                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                  />
-                }>
-                <List.Item.Meta
-                  title={<a href={item.href}>{item.title}</a>}
-                  description={item.description /**sumary here */}
-                />
-              </List.Item>
-            )}
-          />
+          <div>
+            <Tabs
+              defaultActiveKey="1"
+              tabPosition="left"
+              style={{ height: "auto" }}>
+              <TabPane tab="Github Repos" key="1">
+                {this.renderListItem()}
+              </TabPane>
+              <TabPane tab="Articles Written" key="2" />
+              <TabPane tab="Reading Resources" key="3" />
+              <TabPane tab="Code Snippets" key="4" />
+            </Tabs>
+          </div>
         </div>
       </div>
     );
   }
+
+  renderListItem = () => {
+    const { resources } = this.props;
+
+    return (
+      <List
+        itemLayout="vertical"
+        size="large"
+        pagination={{
+          onChange: page => {
+            console.log(page);
+          },
+          pageSize: 10
+        }}
+        dataSource={resources}
+        renderItem={item => (
+          <List.Item
+            key={item.title}
+            actions={[
+              <IconText
+                type="star"
+                text="Fav"
+                style={{ backgroundColor: "yellow" }}
+              />,
+              <IconText type="delete" text="Delete" />,
+              <IconText type="read" text="Read" />,
+              <IconText type="read" text="Unread" />,
+              <HashTags list={["javascript", "facebook"]} />
+            ]}
+            extra={
+              <img
+                width={150}
+                alt="logo"
+                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            }>
+            <List.Item.Meta
+              title={
+                <a href={item.href} target="_target">
+                  {item.title}
+                </a>
+              }
+              description={item.description /**sumary here */}>
+              xs
+            </List.Item.Meta>
+          </List.Item>
+        )}
+      />
+    );
+  };
 
   OnCloseAddResourceForm = () => {
     this.setState({
